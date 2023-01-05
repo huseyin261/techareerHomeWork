cf5133c84d1d505a5674838546dafdf178cca131
huseyin261 <hdemirkoparan26@gmail.com>
2023-01-05 11:07:39 +0300

    Initial Commit

---
+++ Odev1.playground/Contents.swift
@@ -0,0 +1,95 @@
+import UIKit
+
+var sehir: String = "Eskişehir"
+var ulke: String = "Türkiye"
+var telefon: Int = +905303683872
+var postaKodu: Int = 26020
+var email: String = "hdemirkoparan26@gmail.com"
+var meslek: String = "Bilgisayar Mühendisi"
+var stokMiktari: Int = 25
+var musteriAdi: String = "Ahmet"
+var bakiye: Int = 100
+var dogumGunu: String = "23 mart 1997"
+var maas: Int = 15000
+var medeniDurum: String = "Bekar"
+var urunYorumu: String = "Güzel, kullanışlı"
+var odemeTarihi: String = "22/03/2022"
+var odeme: Int = 6500
+var siparisAdeti: Int = 75
+var arabaModeli: Int = 2015
+var kitapAdi: String = "Yüzüklerin Efendisi"
+var yayinlamaTarihi: Int = 1997
+var indirimMiktari: String = "%30"
+var odaSayisi: Int = 4
+var enlem: Int = 350
+var boylam: Int = 250
+var urunAdi: String = "Saat"
+var yemekFiyati: Int = 35
+var marka: String = "Apple"
+var muzikAdi: String = "Metallica - Unforgiven 2"
+var videoSuresi: Double = 62.32
+var urunPuani: Int = 90
+var resimAdi: String = "Monaliza"
+var dosyaFormati: String = "jpg"
+var renk: String = "Kırmızı"
+var renkKodu: String = "#A5B3C4"
+var telefonModeli: String = "iPhone 6s"
+var ekranBoyutu: Double = 11.3
+var agirlik: String = "125 gr"
+var ulusalGun: String = "19 Mayıs"
+var tatilGunu: String = "1 Ocak"
+var rezervasyonTarihi: String = "25 Haziran 2023"
+var sokakAdi: String = "Çağlarca Sokak"
+var otobusHatti: String = "63s"
+var kalanDakika: Int = 50
+var takipKodu: Int = 542984
+var kuponSuresi: String = "15 gün 2 saat 13 dakika"
+var kuponKodu: Int = 243752
+var faturaAdresi: String = "Vişnelik mahallesi Atatürk bulvarı no:191/9"
+
+print("Şehir : \(sehir)")
+print("Ülke : \(ulke)")
+print("Telefon : \(telefon)")
+print("Posta Kodu : \(postaKodu)")
+print("Email : \(email)")
+print("Meslek : \(meslek)")
+print("Stok Miktarı : \(stokMiktari)")
+print("Müşteri Adı : \(musteriAdi)")
+print("Bakiye : \(bakiye)")
+print("Doğum Günü : \(dogumGunu)")
+print("Maaş : \(maas)")
+print("Medeni Durum : \(medeniDurum)")
+print("Ürün Yorum : \(urunYorumu)")
+print("Ödeme Tarihi : \(odemeTarihi)")
+print("Ödeme : \(odeme)")
+print("Sipariş Adeti : \(siparisAdeti)")
+print("Araba Modeli : \(arabaModeli)")
+print("Kitap Adı : \(kitapAdi)")
+print("Yayınlama Tarihi : \(yayinlamaTarihi)")
+print("İndirim Miktarı : \(indirimMiktari)")
+print("Oda Sayısı : \(odaSayisi)")
+print("Enlem : \(enlem)")
+print("Boylam : \(boylam)")
+print("Ürün Adı : \(urunAdi)")
+print("Yemek Fiyatı : \(yemekFiyati)")
+print("Marka : \(marka)")
+print("Müzik Adı : \(muzikAdi)")
+print("Video Süresi : \(videoSuresi)")
+print("Ürün Puanı : \(urunPuani)")
+print("Resim Adı : \(resimAdi)")
+print("Dosya Formatı : \(dosyaFormati)")
+print("Renk : \(renk)")
+print("Renk Kodu : \(renkKodu)")
+print("Telefon Modeli : \(telefonModeli)")
+print("Ekran Boyutu : \(ekranBoyutu)")
+print("Ağırlık : \(agirlik)")
+print("Ulusal Gün : \(ulusalGun)")
+print("Tatil Günü : \(tatilGunu)")
+print("Rezervasyon Tarihi : \(rezervasyonTarihi)")
+print("Sokak Adı : \(sokakAdi)")
+print("Otobüs Hattı : \(otobusHatti)")
+print("Kalan Dakika : \(kalanDakika)")
+print("Takip Kodu : \(takipKodu)")
+print("Kupon Süresi : \(kuponSuresi)")
+print("Kupon Kodu : \(kuponKodu)")
+print("Fatura Adresi : \(faturaAdresi)")
